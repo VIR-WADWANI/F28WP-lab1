ac933d43b2b3b9a73d1b81f52aea6159966f113a
VIR-WADWANI <vyw2001@hw.ac.uk>
2023-11-03 10:05:08 +0400

    commit5

--- Week4/main.js
+++ Week4/main.js
@@ -1,7 +1,16 @@
-var ourRequest = new XMLHttpRequest();
+var cityContainer = document.getElementById("city-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://github.com/VIR-WADWANI/F28WP-lab1/blob/main/Week4/cities1.json');
+    ourRequest.onload = function() {
+        var ourData = JSON.parse(ourRequest.responseText);
+        console.log(ourData[0]);
+    };
+    ourRequest.send();
+})
 
-ourRequest.open('GET', 'https://github.com/VIR-WADWANI/F28WP-lab1/blob/main/Week4/cities1.json');
-ourRequest.onload = function() {
-    console.log(ourRequest.responseText);
-};
-ourRequest.send();
+function renderHTML(data){
+    var htmlString = "this is a test";
+    cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+}
