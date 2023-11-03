0b80c243a8689da7237dff97d001115bab92868f
VIR-WADWANI <vyw2001@hw.ac.uk>
2023-11-03 09:41:34 +0400

    commit2

--- Week4/main.js
+++ Week4/main.js
@@ -1,6 +1,6 @@
 var ourRequest = new XMLHttpRequest();
 
-ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
+ourRequest.open('GET', 'https://github.com/VIR-WADWANI/F28WP-lab1/blob/main/Week4/cities1.json');
 ourRequest.onload = function() {
     console.log(ourRequest.responseText);
 };
